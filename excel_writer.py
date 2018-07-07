08148954f306ddc693d0c8d2edfbdcde3445dcc5
WngShhng <shouheng2015@gmail.com>
2018-07-07 11:03:16 +0800

    add sperated files

---
+++ excel_writer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import xlwt
+
+def write_excel(dist, languages):
+    
+    print('Beginning write resources to excel ......')
+
+    # 创建 Excel 的工作簿
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+
+    # 为每种类型的语言定义一个表单
+    for language in languages:
+        
+        sheet = book.add_sheet(language, cell_overwrite_ok=True)
+
+        # 写表头
+        sheet.write(0, 0, '字符串名称')
+        sheet.write(0, 1, '原义')
+        sheet.write(0, 2, '翻译结果')
+
+        # 写入字典到文件当中
+        row = 1
+        for k, v in dist.items():
+            sheet.write(row, 0, k)
+            sheet.write(row, 1, v)
+            row += 1
+
+        # 保存文件
+        book.save(r'./Translate.xls') 
---
+++ xml_reader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+def strings_reader(path):
+    
+    print('Begin to parse ' + path + ' ......')
+
+    # 使用minidom解析器打开 XML 文档
+    DOMTree = xml.dom.minidom.parse(path)
+    collection = DOMTree.documentElement
+	
+    # 读取所有 string 标签
+    strings = collection.getElementsByTagName("string")
+
+    # 将读取到的所有元素拼接成字典列表
+    dist = {}
+    index = 0
+    for string in strings:
+        index += 1
+        if string.hasAttribute('name'):
+            dist[string.getAttribute('name')] = string.childNodes[0].data
+        else:
+            raise Exception('Invliad string definition at index : ', index)
+
+    print('Successfully parsed ' + path + ' ......')
+
+    # 返回解析结果
+    return dist
