a6f44ceefd2f8c18cbd9a5fa2ea8430c623b105f
WngShhng <shouheng2015@gmail.com>
2018-07-07 12:12:04 +0800

    add

--- translator.py
+++ translator.py
@@ -4,10 +4,10 @@
 __author__ = 'WngShhng'
 
 import xml_reader as xr
+import xml_writer as xw
 import excel_writer as ew
 import excel_reader as er
 
-import xlrd
 
 # 根据指定的文件资源，创建一个用于翻译的 Excel 表格
 def createTranslateResources():
@@ -30,51 +30,9 @@ def createTranslateResources():
 def createTranslatedResources():
 	
 	dsits = er.read_excel(r'./Translate.xls')
-	for res_name, dist in dsits.items():
-		print(res_name)
-		for k,v in dist.items():
-			print(k + ':' + v)
+	
+	xw.write_xmls(dsits, True)
 
-def readExcel():
-	xlsfile = r"./test1.xls" 			# 打开指定路径中的xls文件
-	book = xlrd.open_workbook(xlsfile) 	# 得到Excel文件的book对象，实例化对象
-
-	sheet0 = book.sheet_by_index(0) 	# 通过sheet索引获得sheet对象
-	print(sheet0)						# 输出sheet对象
-	print("==========")
-
-	sheet_name = book.sheet_names()[0]	# 获得指定索引的sheet表名字
-	print(sheet_name)
-	print("==========")
-
-	sheet1 = book.sheet_by_name(sheet_name)	# 通过sheet名字来获取，当然如果知道sheet名字就可以直接指定
-	nrows = sheet0.nrows    				# 获取行总数
-	print(nrows)
-	print("==========")
-
-	for i in range(nrows) :
-		print(sheet1.row_values(i))
-	print("==========")
-
-	ncols = sheet0.ncols
-	print(ncols)
-	print("==========")
-
-	row_data = sheet0.row_values(0)
-	print(row_data)
-	print("==========")
-
-	col_data = sheet0.col_values(0) 
-	print(col_data)
-	print("==========")
-
-	cell_value1 = sheet0.cell_value(0, 0)
-	print(cell_value1)
-	print("==========")
-
-	cell_value2 = sheet0.cell_value(0, 1)
-	print(cell_value2)
-	print("==========")
 
 createTranslatedResources()
 
